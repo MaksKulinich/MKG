88be9202756cdfb7fbeb5aaffb822fcc3c6b5b54
MaksKulinich <113579109+MaksKulinich@users.noreply.github.com>
2022-09-20 14:52:24 +0300

    Add files via upload

---
+++ lab3(2).py
@@ -0,0 +1,9 @@
+import sys
+sideA = float(sys.argv[1])
+sideB = float(sys.argv[2])
+sideC = float(sys.argv[3])
+k = float(sys.argv[4])
+sideA1 = sideA*k
+sideB1 = sideB*k
+sideC1 = sideC*k
+print("Трикутник подібний до даного:", sideA1, sideB1, sideC1)
